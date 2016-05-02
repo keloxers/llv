67126b71c85ea6bc09daa75fab169b67c27a9570
Miguel Mendez <keloxers@gmail.com>
2016-05-02 11:09:32 -0300

    push

--- app/views/articulos/ver.blade.php
+++ app/views/articulos/ver.blade.php
@@ -2,116 +2,55 @@
 
 @section('content')
 
-<link rel="stylesheet" type="text/css" href="/css/tables.css"/>
 
-<div id="page"><!-- - - - - - - - - - SECTION - - - - - - - - - -->
-<div class="pi-section-w pi-section-white piICheck piStylishSelect">
-	<div class="pi-section pi-padding-bottom-80">
 
-		<div class="pi-text-center pi-margin-bottom-60">
-			<h1 class="pi-uppercase pi-weight-700 pi-has-border pi-has-tall-border pi-has-short-border pi-margin-bottom-70">
-				Articulos
-			</h1>
+<!-- SHORTCODE-ACCORDION -->
+<div class="container shortcode-content">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="uppercase">Articulos</h3>
+			 <a href='/articulos/create'>Nuevo articulo</a><br><br>
+			<div class="accordionf">
+
+				<?php
+
+								if (count($articulos)>0 )  {
+
+
+									foreach ($articulos as $articulo)
+										{
+
+												$categoria = Categoria::find($articulo->categorias_id);
+
+												echo "<h3>$articulo->articulo</h3>";
+
+												echo "<div class='pane'>";
+
+												echo "<p>";
+												echo "Copete:" . $articulo->copete . "<br>";
+												echo "Tipo:" . $articulo->tipo . "<br>";
+												echo "Estado:" . $articulo->estado . "<br>";
+
+												echo "<a href='/articulos/" . $articulo->id . "/publicar' class='btn-small color4'>Publicar</a> ";
+												echo "<a href='/articulos/" . $articulo->id . "/edit' class='btn-small color4'>Editar</a> ";
+												echo "<a href='/articulos/" . $articulo->id . "/archivos/articulo' class='btn-small color4'>Archivos</a> ";
+												echo "<a href='/articulos/" . $articulo->id . "/delete' class='btn-small color4'>Eliminar</a> ";
+
+												echo "</p>";
+												echo "</div>";
+
+											}
+
+									}
+							?>
+
+
+			</div>
 		</div>
-
-
-
-	<?php
-		echo "<a href='/articulos/create' class='btn pi-btn-base pi-btn-small'>Nuevo articulo</a><br><br>";
-
-		if (count($articulos)>0 )  {
-
-
-?>
-
-										<div class="pi-responsive-table-sm">
-													<table class="pi-table pi-table-complex pi-table-hovered pi-round pi-table-shadow pi-table-all-borders">
-
-														<!-- Table head -->
-														<thead>
-
-													<!-- Table row -->
-													<tr>
-														<th>
-															Titular
-														</th>
-														<th>
-															Copete
-														</th>
-														<th>
-															Tipo
-														</th>
-														<th>
-															Estado
-														</th>
-
-														<th>
-															Accion
-														</th>
-													</tr>
-													<!-- End table row -->
-
-												</thead>
-												<!-- End table head -->
-
-												<!-- Table body -->
-												<tbody>
-
-												<?php
-
-											foreach ($articulos as $articulo)
-												{
-
-														$categoria = Categoria::find($articulo->categorias_id);
-
-														echo "<tr>";
-												    echo "<td>" . $articulo->articulo . "</td>";
-												    echo "<td>" . $articulo->copete . "</td>";
-														echo "<td>" . $articulo->tipo . "</td>";
-												    echo "<td>" . $articulo->estado . "</td>";
-														echo "<td>" ;
-
-														echo "<a href='/articulos/" . $articulo->id . "/publicar' class='btn pi-btn-base pi-btn-small'>Publicar</a> ";
-														echo "<a href='/articulos/" . $articulo->id . "/edit' class='btn pi-btn-base pi-btn-small'>Editar</a> ";
-														echo "<a href='/articulos/" . $articulo->id . "/archivos/articulo' class='btn pi-btn-base pi-btn-small'>Archivos</a> ";
-														echo "<a href='/articulos/" . $articulo->id . "/delete' class='btn pi-btn-base pi-btn-small'>Eliminar</a> ";
-
-
-														print "</td>";
-														print "</tr>";
-
-
-
-												}
-
-
-											?>
-
-
-											</tbody>
-											<!-- End table body -->
-
-										</table>
-									</div>
-									<!-- End table -->
-
-									<div class="pi-pagenav pi-text-center">
-										{{ $articulos->links()}}
-									</div>
-
-
-
-
-							</footer>
-						</section>
-		<?php
-
-			}
-
-		?>
-
+	</div>
 </div>
-</div>
-</div>
+
+{{ $articulos->links()}}
+
 
 @stop
